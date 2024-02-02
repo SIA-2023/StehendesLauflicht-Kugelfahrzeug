c7053b02f4b783238e0675263bd3ca536226a01d
Peanutt42 <p3anutt42@gmail.com>
2024-02-02 16:47:50 +0100

    leds funktionieren - compiler bug gefixed
    BUG: bool flip wurde vom compiler wegoptimisiert und ist danach nur true

--- StehendesLauflicht-Kugelfahrzeug.ino
+++ StehendesLauflicht-Kugelfahrzeug.ino
@@ -1,7 +1,11 @@
 // Autor: Peter
 
 #include <Adafruit_NeoPixel.h>
-#include "Schachbrett.hpp"
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#include "Vec2.hpp"
 
 #define NEO_PIXEL_PIN 11
 #define NEO_PIXEL_LED_COUNT 24
@@ -43,20 +47,39 @@ Vec2 lichter_abtaende[NEO_PIXEL_LED_COUNT] = {
   Vec2(60.f, -60.f),
 };
 
-Vec2 auto_positino;
+Vec2 auto_position;
 float auto_orientation = 0.f;
 
-Schachbrett schachbrett(Adafruit_NeoPixel::Color(255, 255, 255), Adafruit_NeoPixel::Color(100, 100, 100), 10.f);
+using Farbe = uint32_t;
+Farbe schachbrett_farbe(const Vec2& pos, float scale, Farbe farbe1, Farbe farbe2);
 
 void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  Serial.begin(9600);
+  Serial.println("Hello");
+
   neo_pixel.begin();
+  neo_pixel.setBrightness(100);
+  neo_pixel.clear();
 }
 
 void loop() {
-  for (int i = 0; i < NEO_PIXEL_LED_COUNT; i++) {
-    Vec2 licht_position = auto_positino + lichter_abtaende[i].rotiert(auto_orientation);
-    neo_pixel.setPixelColor(i, schachbrett.farbe_bei_punkt(licht_position));
-  }
+  neo_pixel.clear();
   
+  const Farbe farbe1 = neo_pixel.Color(255, 255, 255);
+  const Farbe farbe2 = neo_pixel.Color(0, 0, 0);
+  for (int i = 0; i < NEO_PIXEL_LED_COUNT; i++) {
+    Vec2 licht_position = auto_position + lichter_abtaende[i].rotiert(auto_orientation);
+    neo_pixel.setPixelColor(i, schachbrett_farbe(licht_position, 30.f, farbe1, farbe2));
+  }
   neo_pixel.show();
+}
+
+Farbe schachbrett_farbe(const Vec2& pos, float scale, Farbe farbe1, Farbe farbe2) {
+  // 'volatile' MUSS hier sein, ansonsten wird flip wegoptimisiert und ist immer true
+  // ähnlicher bug: https://stackoverflow.com/questions/67359667/how-to-prevent-c-compiler-optimising-variable
+  volatile bool flip = ((int)(pos.x / 30.f) + (int)(pos.y / 30.f)) % 2 == 0;
+  return flip ? farbe1 : farbe2;
 }
