eea27184b00573fd01a745e380e109c97f3e77ff
Peanutt42 <p3anutt42@gmail.com>
2024-02-09 15:33:52 +0100

    Abstände der Lichter angepasst

--- StehendesLauflicht-Kugelfahrzeug.ino
+++ StehendesLauflicht-Kugelfahrzeug.ino
@@ -1,51 +1,12 @@
 // Autor: Peter
 
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
 
 #include "Vec2.hpp"
+#include "AutoConfig.hpp"
 
-#define NEO_PIXEL_PIN 11
-#define NEO_PIXEL_LED_COUNT 24
 
 Adafruit_NeoPixel neo_pixel(NEO_PIXEL_LED_COUNT, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-// abstand aller lichter zu der Mitte des Autos
-Vec2 lichter_abtaende[NEO_PIXEL_LED_COUNT] = {
-  // vorne
-  Vec2(60.f, -60.f),
-  Vec2(36.6f, -60.f),
-  Vec2(13.3f, -60.f),
-  Vec2(-13.3f, -60.f),
-  Vec2(-36.6f, -60.f),
-  Vec2(-60.f, -60.f),
-
-  // hinten
-  Vec2(60.f, 60.f),
-  Vec2(36.6f, 60.f),
-  Vec2(13.3f, 60.f),
-  Vec2(-13.3f, 60.f),
-  Vec2(-36.6f, 60.f),
-  Vec2(-60.f, 60.f),
-
-  // links
-  Vec2(-60.f, 60.f),
-  Vec2(-60.f, 36.6f),
-  Vec2(-60.f, 13.3f),
-  Vec2(-60.f, -13.3f),
-  Vec2(-60.f, -36.6f),
-  Vec2(-60.f, -60.f),
-
-  // rechts
-  Vec2(60.f, 60.f),
-  Vec2(60.f, 36.6f),
-  Vec2(60.f, 13.3f),
-  Vec2(60.f, -13.3f),
-  Vec2(60.f, -36.6f),
-  Vec2(60.f, -60.f),
-};
 
 Vec2 auto_position;
 float auto_orientation = 0.f;
@@ -54,9 +15,6 @@ using Farbe = uint32_t;
 Farbe schachbrett_farbe(const Vec2& pos, float scale, Farbe farbe1, Farbe farbe2);
 
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
   Serial.begin(9600);
   Serial.println("Hello");
 
@@ -66,13 +24,16 @@ void setup() {
 }
 
 void loop() {
+  auto_position.y += 0.1f;
+  delay(10);
+  
   neo_pixel.clear();
   
   const Farbe farbe1 = neo_pixel.Color(255, 255, 255);
   const Farbe farbe2 = neo_pixel.Color(0, 0, 0);
   for (int i = 0; i < NEO_PIXEL_LED_COUNT; i++) {
     Vec2 licht_position = auto_position + lichter_abtaende[i].rotiert(auto_orientation);
-    neo_pixel.setPixelColor(i, schachbrett_farbe(licht_position, 30.f, farbe1, farbe2));
+    neo_pixel.setPixelColor(i, schachbrett_farbe(licht_position, 10.f, farbe1, farbe2));
   }
   neo_pixel.show();
 }
@@ -80,6 +41,6 @@ void loop() {
 Farbe schachbrett_farbe(const Vec2& pos, float scale, Farbe farbe1, Farbe farbe2) {
   // 'volatile' MUSS hier sein, ansonsten wird flip wegoptimisiert und ist immer true
   // ähnlicher bug: https://stackoverflow.com/questions/67359667/how-to-prevent-c-compiler-optimising-variable
-  volatile bool flip = ((int)(pos.x / 30.f) + (int)(pos.y / 30.f)) % 2 == 0;
+  volatile bool flip = ((int)(pos.x / scale) + (int)(pos.y / scale)) % 2 == 0;
   return flip ? farbe1 : farbe2;
 }
